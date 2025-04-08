a5aad92b106706fb85ebd0e396206ca7163dcb15
crookler <ethancrook03@gmail.com>
2025-04-08 02:33:56 -0400

    Improve acceptance criteria

--- src/environment_map/src/a_star_search.cpp
+++ src/environment_map/src/a_star_search.cpp
@@ -1,7 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
-#include "path_interfaces/action/a_search_query.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
+
+#include "path_interfaces/msg/flat_grid.hpp"
+#include "path_interfaces/action/a_search_query.hpp"
+#include "path_interfaces/msg/path_query.hpp"
 
 namespace environment_mapping {
 
@@ -22,13 +25,21 @@ public:
 
 private:
     rclcpp_action::Server<ASearchQuery>::SharedPtr action_server;
+    rclcpp::Subscription<path_interfaces::msg::FlatGrid>::SharedPtr reduced_grid_subscriber;
 
     rclcpp_action::GoalResponse handle_goal(
         const rclcpp_action::GoalUUID & uuiud, 
         std::shared_ptr<const ASearchQuery::Goal> goal) {
-            void(uuid); // Unused
-            RCLCPP_INFO(this->get_logger(), "Navigation goal has ID: %d", goal->query.goal.header.seq);
-            return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE; // Just accept all goals for now (may add in some sugar to detect if goal is within map bounds)
+            const std::string& frame = goal->query.goal.header.frame_id; // Read only
+            RCLCPP_INFO(this->get_logger(), "Received goal with frame_id: %s", frame.c_str());
+            
+            // Only accept goal that is set in the global frame (map)
+            if (frame != "map") {
+                RCLCPP_WARN(this->get_logger(), "Rejected goal due to invalid frame_id: %s", frame.c_str());
+                return rclcpp_action::GoalResponse::REJECT;
+            }
+
+            return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
 
     rclcpp_action::CancelResponse handle_cancel(
@@ -38,14 +49,16 @@ private:
     }
 
     void handle_accepted(
-        cost std::shared_ptr<GoalHandlePath> goal_handle) {
-            std::thread{std::bind(&AStarSearch::execute, this, std::placeholders::_1), goal_handle}.detach() // Allow thread to run in parallel
+        const std::shared_ptr<GoalHandlePath> goal_handle) {
+            // Allow thread to run in parallel
+            std::thread{std::bind(&AStarSearch::execute, this, std::placeholders::_1), goal_handle}.detach();
     }
 
     void execute(
         const std::shared_ptr<GoalHandlePath> goal_handle) {
-            //TODO: A* search
+        // A C++ Program to implement A* Search Algorithm https://www.geeksforgeeks.org/a-search-algorithm/
     }
+
 };
 
 }
